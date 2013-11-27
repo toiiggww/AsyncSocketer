d58a12056cbd81cace6780c8e74c99ddd3adcaf3
toiiggww <toiiggww@163.com>
2013-11-27 18:09:49 +0800

    Update 4 : AnsycServer

---
+++ PartialSocketServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsyncSocketer
+{
+    class PartialSocketServer:PartialSocket
+    {
+        protected object evtAccept;
+        public event SocketEventHandler Accept { add { base.Events.AddHandler(evtAccept, value); } remove { base.Events.RemoveHandler(evtAccept, value); } }
+    }
+}
